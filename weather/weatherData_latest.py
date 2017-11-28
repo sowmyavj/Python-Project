aa2f7f618170a552c8d518678213d81198a18197
sowmyavj <sowmyav@sowmyas-MacBook-Air.local>
2017-11-28 18:21:17 -0500

    added hour,min

--- weather/weatherData_latest.py
+++ weather/weatherData_latest.py
@@ -8,9 +8,19 @@ Created on Wed Nov  8 00:59:51 2017
 import urllib2
 from bs4 import BeautifulSoup
 
+def getPMHour(_hour):
+	if(_hour=='12'):
+		return "12"
+	return (int(_hour) +12)
+
+def getAMHour(_hour):
+	if(_hour=='12'):
+		return "00"
+	return (int(_hour))
+
 
 f=open("climate_data_ny.csv",'w');
-f.write("Timestamp,Hour,Temp,Conditions,Weather Sentiment"+'\n')
+f.write("Date,Hour,Min,Temp,Conditions,Weather Sentiment"+'\n')
 
 #Change the range according to month range required. eg: For month of Jan and feb use range(1,3)
 for m in range(1,13):
@@ -51,23 +61,39 @@ for m in range(1,13):
         for temp in wx:
             conditions= temp.find_all('td')[index_of_conditions].text.encode('utf-8').strip()
             if conditions in ["Clear","Light Rain"]:
-                weather_sentiment="positive"
+                weather_sentiment=1
             elif conditions in ["Partly Cloudy","Haze","Scattered Clouds","Fog","Mist","Unknown"]:
-                weather_sentiment="neutral"
+                weather_sentiment=0
             elif conditions in ["Light Snow","Mostly Cloudy","Overcast","Snow","Rain","Heavy Rain","Heavy Snow","Light Freezing Rain","Light Freezing Fog"]:
-                weather_sentiment="negative"
+                weather_sentiment=-1
             else:
                 weather_sentiment=conditions
 
-            hour=  temp.find_all('td')[0].text
+            hour_text=  temp.find_all('td')[0].text
+            #print hour_text
+            hoursplit =hour_text.split();
+            hour_min= hoursplit[0].strip()
+            am_pm=hoursplit[1].strip()
+            hour=hour_min.split(":")[0]
+            minute=hour_min.split(":")[1]
+
+            
+            if (am_pm == 'PM'):
+            	hour=str(getPMHour(hour))
+            elif(am_pm == 'AM'):
+            	hour=str(getAMHour(hour))
+           
+           	
+            #print "hr:"+hour
+            #print "min:"+min
             #temperature=  temp.find_all('td')[1].find_all(attrs={"class":"wx-value"})[0].text
             temperature = "NA" if (temp.find_all('td')[1].find_all(attrs={"class":"wx-value"})==[]) else temp.find_all('td')[1].find_all(attrs={"class":"wx-value"})[0].text
             #print temperature
             #print temp.find_all('td')[0].text,temp.find_all('td')[1].text,temp.find_all('td')[12].text
-            date='2017'+mStamp+dStamp
+            date=mStamp+'/'+dStamp+'/2017'
             #f.write(date+','+str(hour)+','+str(temp)+','+str(conditions)+'\n')
-            f.write(date+','+hour.encode('utf-8').strip()+','+temperature.encode('utf-8').strip()+','+str(conditions)+','+str(weather_sentiment)+'\n')
-
+            # f.write(date+','+hour.encode('utf-8').strip()+','+temperature.encode('utf-8').strip()+','+str(conditions)+','+str(weather_sentiment)+'\n')
+            f.write(date+','+hour.encode('utf-8').strip()+','+minute.encode('utf-8').strip()+','+temperature.encode('utf-8').strip()+','+str(conditions)+','+str(weather_sentiment)+'\n')
 f.close()
 
 '''
